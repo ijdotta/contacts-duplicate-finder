dd779c3102f0c7eb978aad3bbeb2d0a109b8075e
Ignacio Dotta <ij.dotta@gmail.com>
2024-12-04 14:45:35 -0300

    impl injector

---
+++ src/main/java/com/main/Injector.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import com.main.matcher.ContactsMatcher;
+import com.main.matcher.ContactsMatcherImpl;
+import com.main.repository.ContactsRepository;
+import com.main.repository.ContactsRepositoryImpl;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class Injector {
+
+    static class Holder {
+        static final Injector INSTANCE = new Injector();
+    }
+
+    public static Injector getInjector() {
+        return Holder.INSTANCE;
+    }
+
+    private static final String FILE_PATH = "src/main/resources/contacts.txt";
+    private final ContactsRepository repository = new ContactsRepositoryImpl(FILE_PATH);
+    private final ContactsMatcher matcher = new ContactsMatcherImpl();
+    @Getter private final DuplicatesContactsFinder duplicatesContactsFinder = new DuplicatesContactsFinderImpl(repository, matcher);
+}
