426e925b6adc0153a80a2ec804506c2d0f4e8eda
Ignacio Dotta <ij.dotta@gmail.com>
2024-12-05 11:46:31 -0300

    remove extra matches

--- src/main/java/com/main/DuplicatesContactsFinderImpl.java
+++ src/main/java/com/main/DuplicatesContactsFinderImpl.java
@@ -10,6 +10,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Finds duplicates contacts in the contacts repository.
@@ -22,16 +24,26 @@ public class DuplicatesContactsFinderImpl implements DuplicatesContactsFinder {
 
     @Override
     public List<Match> findDuplicates() {
+        calculateMatches();
+        removeExtraMatches();
+        return matches;
+    }
+
+    private void calculateMatches() {
         List<Contact> contacts = repository.getContacts();
         for (int i = 0; i < contacts.size(); i++)
             for (int j = i + 1; j < contacts.size(); j++)
                 tryToMatch(contacts.get(i), contacts.get(j));
-        return matches;
     }
 
     private void tryToMatch(Contact reference, Contact candidate) {
         Optional.ofNullable(matcher.calculateMatch(reference, candidate))
                 .map(accuracy -> new Match(reference.id(), candidate.id(), accuracy))
                 .ifPresent(matches::add);
+    }
+
+    private void removeExtraMatches() {
+        Set<Integer> matchesIDs = matches.stream().map(Match::matchId).collect(Collectors.toSet());
+        matches.removeIf(match -> matchesIDs.contains(match.sourceId()));
     }
 }
